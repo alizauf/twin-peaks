e87ecafbde125c27a1131d5232a59dffba501d21
alizauf <aliza.aufrichtig@gmail.com>
2016-02-10 15:08:40 -0500

    standardized locations

--- scraping.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-#import csv
-
-
-import urllib
-r = urllib.urlopen('http://www.glastonberrygrove.net/texts/script8.html').read()
-soup = BeautifulSoup(r, 'html.parser')
-
-str.format(soup.prettify())
